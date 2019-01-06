eae1dfa584cee2c46ad92d6c2923b5adb74849cb
sameer ranjan <40149707+wilfredarin@users.noreply.github.com>
2019-01-06 19:24:17 +0530

    Update Merge_Sort.py

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -1,6 +1,16 @@
 
 
 ''' devide  n conquor algo   '''
+"""
+1) Initialize count as 0
+2) Sort all numbers in increasing order.
+3) Remove duplicates from array.
+4) Do following for each element arr[i]
+   a) Binary Search for arr[i] + k in subarray from i+1 to n-1.
+   b) If arr[i] + k found, increment count. 
+5) Return count. 
+
+"""
 
 def mergeSort(alist):
     print("Splitting ",alist)
@@ -43,6 +53,52 @@ def mergeSort(alist):
 alist = [54,26,93,17,77,31,44,55,20]
 mergeSort(alist)
 print(alist)
+
+
+
+"""
+******************************************************************************************************************
+result delete this whole comment   
+
+
+
+('Splitting ', [54, 26, 93, 17, 77, 31, 44, 55, 20])
+('Splitting ', [54, 26, 93, 17])
+('Splitting ', [54, 26])
+('Splitting ', [54])
+('Merging ', [54])
+('Splitting ', [26])
+('Merging ', [26])
+('Merging ', [26, 54])
+('Splitting ', [93, 17])
+('Splitting ', [93])
+('Merging ', [93])
+('Splitting ', [17])
+('Merging ', [17])
+('Merging ', [17, 93])
+('Merging ', [17, 26, 54, 93])
+('Splitting ', [77, 31, 44, 55, 20])
+('Splitting ', [77, 31])
+('Splitting ', [77])
+('Merging ', [77])
+('Splitting ', [31])
+('Merging ', [31])
+('Merging ', [31, 77])
+('Splitting ', [44, 55, 20])
+('Splitting ', [44])
+('Merging ', [44])
+('Splitting ', [55, 20])
+('Splitting ', [55])
+('Merging ', [55])
+('Splitting ', [20])
+('Merging ', [20])
+('Merging ', [20, 55])
+('Merging ', [20, 44, 55])
+('Merging ', [20, 31, 44, 55, 77])
+('Merging ', [17, 20, 26, 31, 44, 54, 55, 77, 93])
+[17, 20, 26, 31, 44, 54, 55, 77, 93]
+"""
+
 
 
 
